13a800970c371cdcbec5f07973982666c33cfaab
Eduard <edza92@mail.ru>
2021-01-28 22:05:53 +0500

    homework 4.4.1 and 4.4.2 done

---
+++ homework_4.4/Alphabet/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        for(int i =0; i < alphabet.length(); i ++){
+            char symbol = alphabet.charAt(i);
+            int symbolCode = (int) symbol;
+            System.out.println(alphabet.charAt(i) + ": " + symbolCode);
+        }
+        String rusAlphabet = "АаБбВвГгДдЕеЖжЗзИиЙйКкЛлНнОоПпРрСсТтУуФфХхЦцЧчЩщШшьыъЭэЮюЯя";
+        for(int i =0; i < rusAlphabet.length(); i ++){
+            char symbol = rusAlphabet.charAt(i);
+            int symbolCode = (int) symbol;
+            System.out.println(rusAlphabet.charAt(i) + ": " + symbolCode);
+        }
+
+    }
+}
--- homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,26 @@
+import static java.lang.Integer.*;
+
 public class Main {
 
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
+    int vasyaEarnLeft = text.indexOf("л") + 1;
+    int vasyaEarnRight = text.indexOf(" ", text.indexOf("л") + 2);
+    String vasyaEarn = (text.substring(vasyaEarnLeft, vasyaEarnRight)).trim();
+    //System.out.println(vasyaEarn);
+    int petyaEarnLeft = text.indexOf("-") + 1;
+    int petyaEarnRight = text.indexOf(" ", petyaEarnLeft + 2);
+    String petyaEarn = (text.substring(petyaEarnLeft, petyaEarnRight)).trim();
+    //System.out.println(petyaEarn);
+    int mashaEarnLeft = text.lastIndexOf("-");
+    int mashaEarnRight = text.lastIndexOf("р");
+    String mashaEarn = (text.substring(mashaEarnLeft + 1, mashaEarnRight)).trim();
+    //System.out.println(mashaEarn);
+    int moneyAmount = parseInt(petyaEarn) + parseInt(vasyaEarn) + parseInt(mashaEarn);
+    System.out.println(moneyAmount);
+
   }
 
 }
