16b843507814a831e3e53dc2fc10d2d585918d66
edza92@mail.ru <edza92@mail.ru>
2021-01-28 14:24:05 +0500

    Homework 4.3 done

--- homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -7,6 +7,31 @@ public class Main {
         String boxes = scanner.nextLine();
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+        int boxesQuantity = Integer.parseInt(boxes);
+        int containerQuantity = (int) Math.ceil((double) boxesQuantity / 27);
+        int cargoQuantity = (int) Math.ceil((double) containerQuantity / 12);
+        int emptyContainerQuantity = containerQuantity;
+        int emptyBoxesQuantity = boxesQuantity;
+        for (int i = 1; i <= cargoQuantity ; i ++){
+            System.out.println("Грузовик: " + i);
+            for (int j = 1; j <= 12; j++){
+                emptyContainerQuantity--;
+                System.out.println("\tКонтейнер: " + (containerQuantity - emptyContainerQuantity));
+                for (int k = 1; k <= 27; k++){
+                    if (emptyBoxesQuantity == 0){
+                        break;
+                    }
+                    emptyBoxesQuantity--;
+                    System.out.println("\t\tЯщик: " + (boxesQuantity - emptyBoxesQuantity));
+                }
+                if (emptyContainerQuantity == 0){
+                    break;
+                }
+            }
+        }
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + cargoQuantity + " шт.");
+        System.out.println("контейнеров - " + containerQuantity + " шт.");
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
 
