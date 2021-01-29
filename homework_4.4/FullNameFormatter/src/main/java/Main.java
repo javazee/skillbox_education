cd06dc7f47905b33e8080a2f24a3f1d50f2afcff
edza92@mail.ru <edza92@mail.ru>
2021-01-29 17:07:03 +0500

    4.4.3 done and 4.4.4 started

--- homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -5,14 +5,35 @@ public class Main {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
     while (true) {
-      String input = scanner.nextLine();
       if (input.equals("0")) {
         break;
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
+      if (input.indexOf("0") != -1 || input.indexOf("1") != -1 || input.indexOf("2") != -1 || input.indexOf("3") != -1 ||
+              input.indexOf("4") != -1 || input.indexOf("5") != -1 || input.indexOf("6") != -1 || input.indexOf("7") != -1 ||
+              input.indexOf("8") != -1 || input.indexOf("9") != -1) {
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      }
+      int secondSpace = input.indexOf(" ", input.trim().indexOf(" ") + 2);
+      if (secondSpace == input.trim().lastIndexOf(" ")) {
+        if (secondSpace != -1) {
+          String surname = input.trim().substring(0, input.trim().indexOf(" "));
+          String name = input.trim().substring(input.trim().indexOf(" ") +1, secondSpace);
+          String patronymic = input.substring(secondSpace).trim();
+          System.out.printf("Фамилия: %s%nИмя: %s%nОтчество: %s", surname, name, patronymic);
+          break;
+        } else {
+          System.out.println("Введенная строка не является ФИО");
+        }
+      } else {
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      }
+      break;
+    }
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
   }
-
-}
--- homework_4.4/MaskingInformation/src/main/java/Main.java
+++ homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912" , "***");
+        System.out.println(safe);
 
     }
-
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
+        if ((text.indexOf("<") == -1 && text.indexOf(">") == -1) || text.indexOf("<") == -1 || text.indexOf(">") == -1){
+            return text;
+        } else {
+            String initialText = text;
+            text = "";
+            int leftSign = 0;
+            int rightSign;
+            for (int i = 0; i < text.length(); i++) {
+                if (initialText.indexOf("<") == i){
+                    rightSign = i;
+                    text = text + initialText.substring(leftSign + 1, rightSign +1);
+                }
+                if (initialText.indexOf(">") == i) {
+                    text = text + placeholder;
+                    leftSign = i;
+                }
+            }
+            return text;
+        }
     }
-
 }
