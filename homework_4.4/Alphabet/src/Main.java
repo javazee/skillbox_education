0ac0e878726bee5822b7459cfe30cb4e5623f87b
Eduard <edza92@mail.ru>
2021-01-31 11:15:44 +0500

    4.4.1 was amended

--- homework_4.4/Alphabet/src/Main.java
+++ homework_4.4/Alphabet/src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i =0; i < "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz".length(); i ++){
-            System.out.println("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz".charAt(i) + ": " + (int) "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz".charAt(i));
+        for(char i = 'a'; i <= 'z'; i++){
+            System.out.println(i + ": " + (int) i);
         }
-
-        String rusAlphabet = "АаБбВвГгДдЕеЖжЗзИиЙйКкЛлНнОоПпРрСсТтУуФфХхЦцЧчЩщШшьыъЭэЮюЯя";
-        for(int i =0; i < rusAlphabet.length(); i ++){
-            char symbol = rusAlphabet.charAt(i);
-            int symbolCode = (int) symbol;
-            System.out.println(rusAlphabet.charAt(i) + ": " + symbolCode);
+        for(char i = 'A'; i <= 'Z'; i++){
+            System.out.println(i + ": " + (int) i);
+        }
+        for(char i = 'а'; i <= 'я'; i++){
+            System.out.println(i + ": " + (int) i);
+        }
+        for(char i = 'А'; i <= 'Я'; i++){
+            System.out.println(i + ": " + (int) i);
         }
     }
 }
