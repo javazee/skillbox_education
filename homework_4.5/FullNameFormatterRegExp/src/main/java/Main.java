bbf8eed9cc9135090ae1fb128aa572652daa4e93
Eduard <edza92@mail.ru>
2021-01-31 22:43:15 +0500

    4.5 started

--- homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -11,6 +13,24 @@ public class Main {
         break;
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
+      String regex = "(\s+)?[а-яА-ЯЁё](([а-яА-ЯЁё]+)?[-][а-яА-ЯЁё])?([а-яА-ЯЁё]+)?\s+" +    //фамилия, может содержать тире или быть из одной буквы
+              "[а-яА-ЯЁё](([а-яА-ЯЁё]+)?[-][а-яА-ЯЁё]?)?[а-яА-ЯЁё]+\s+" +                   //имя, может быть с тире, минимум 2 символа
+              "[а-яА-ЯЁё](([а-яА-ЯЁё]+)?[-][а-яА-ЯЁё]?)?[а-яА-ЯЁё]+(\s+)?";                 //отчество, может быть с тире, миннимум 2 символа
+      Pattern familyPattern = Pattern.compile("[а-яА-ЯЁё].+?\s");
+      Matcher family = familyPattern.matcher(input);
+      if (Pattern.matches(regex, input)){
+        System.out.println("да");
+        System.out.println(family.group());
+        if (family.find( )) {
+          System.out.println("Фамилия: " + family.group(0) );
+          System.out.println("Имя: " + family.group(1) );
+          System.out.println("Отчество: " + family.group(2) );
+        }else {
+          System.out.println("NO MATCH");
+        }
+      } else {
+        System.out.println("Введенная строка не является ФИО");
+      }
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
   }
--- homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,22 @@
 public class Main {
 
   public static void main(String[] args) {
-
+    int sum = calculateSalarySum(" Вася заработал 50000р, Катя - 30000 р., а Коля 56000");
+    System.out.println(sum);
   }
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
-    return -1;
+    String onlyDigit = (text.replaceAll("[^0-9]"," ")).trim();
+    if (onlyDigit.equals("")){
+      return 0;
+    }
+    String[] salaries = onlyDigit.split("\\s+");
+    int salarySum = 0;
+    for (int i = 0; i < salaries.length; i++) {
+      salarySum = salarySum + Integer.parseInt(salaries[i]);
+    }
+    return salarySum;
   }
 
 }
