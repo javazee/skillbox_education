0334a73eceb3dcda875bc6519c425e1ed955ff32
Eduard <edza92@mail.ru>
2021-01-31 11:55:36 +0500

    added russian symbol "ё"

--- homework_4.4/Alphabet/src/Main.java
+++ homework_4.4/Alphabet/src/Main.java
@@ -7,9 +7,15 @@ public class Main {
             System.out.println(i + ": " + (int) i);
         }
         for(char i = 'а'; i <= 'я'; i++){
+            if ( i =='ж'){
+                System.out.println('ё' + ": " + (int) 'ё');
+            }
             System.out.println(i + ": " + (int) i);
         }
         for(char i = 'А'; i <= 'Я'; i++){
+            if ( i =='Ж'){
+                System.out.println('Ё' + ": " + (int) 'Ё');
+            }
             System.out.println(i + ": " + (int) i);
         }
     }
