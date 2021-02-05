d50b037ad84bcf3eded3f765cdd6233c3955d64b
Eduard <edza92@mail.ru>
2021-02-05 14:33:00 +0500

    homework 4.6 done

--- homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -1,11 +1,16 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
+        int day = 18;
         int month = 12;
-        int year = 1990;
+        int year = 1992;
 
         System.out.println(collectBirthdays(year, month, day));
 
@@ -14,9 +19,20 @@ public class Main {
     public static String collectBirthdays(int year, int month, int day) {
 
         //TODO реализуйте метод для построения строки в следующем виде
+        Calendar myBirthday = new GregorianCalendar(year, month - 1, day);
+        Calendar calendar = new GregorianCalendar();
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy - EE", Locale.ENGLISH);
+        //DateFormat df = new SimpleDateFormat("dd.MM.yyyy - EEEE"); //вариант с выводом на русском
+        DateFormat dayFormat = new SimpleDateFormat("EEE", Locale.ENGLISH);
+        String listOfBirthday = "";
+        int i = 0;
+        while (myBirthday.getTime().before(calendar.getTime())){
+            listOfBirthday += i + " - " + df.format(myBirthday.getTime()) + System.lineSeparator();
+            myBirthday.add(Calendar.YEAR, 1);
+            i++;
+        }
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+        return listOfBirthday;
     }
 }
