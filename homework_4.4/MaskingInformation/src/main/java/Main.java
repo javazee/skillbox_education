6feb5691e3576c56565600fc8dfa45fd0324a2d3
edza92@mail.ru <edza92@mail.ru>
2021-02-10 16:07:53 +0500

    third way of masking information

--- homework_4.4/MaskingInformation/src/main/java/Main.java
+++ homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,10 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        String safe = searchAndReplaceDiamonds("Номер кредитной карты <<5896> <4668> <8898>> 5544" , "***");
-        String example1 = searchAndReplaceDiamonds("Номер кредитной карты <<5896>> 4668 <<8898>> 5544" , "***");
-        String example2 = searchAndReplaceDiamonds("<5686>>" , "***");
-        String example3 = searchAndReplaceDiamonds("<<5686>>" , "***");
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <<5896> <4668> <8898>> 5544", "***");
+        String example1 = searchAndReplaceDiamonds("Номер кредитной карты <<5896>> 4668 <<8898>> 5544", "***");
+        String example2 = searchAndReplaceDiamonds("<<5686>", "***");
+        String example3 = searchAndReplaceDiamonds("<<5686>>", "***");
         String example4 = searchAndReplaceDiamonds("номер <8888 < 5555 > 8888 > some text <5555>", "***");
 
         System.out.println(safe);
@@ -15,41 +13,46 @@ public class Main {
         System.out.println(example3);
         System.out.println(example4);
     }
+
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        // проверка на соответствие количества левых скобочек количеству правых
-        ArrayList<Integer> leftBrace= new ArrayList<>();
-        ArrayList<Integer> rightBrace= new ArrayList<>();
-        for (int i = 0; i < text.length(); i++){
-            if (String.valueOf(text.charAt(i)).equals("<")){
-                leftBrace.add(i);
-            }
-            if (String.valueOf(text.charAt(i)).equals(">")){
-                rightBrace.add(i);
+        int count = 0;
+        String initialText = text;
+        text = "";
+        State.setState(count);//не пойму, почему программа не работает без этой строки
+        for (int i = 0; i < initialText.length(); i++) {
+            if (String.valueOf(initialText.charAt(i)).equals("<")) {
+                count++;
+                State.setState(count);
+            } else if (String.valueOf(initialText.charAt(i)).equals(">")) {
+                count--;
+                if (count < 0){
+                    text = initialText;
+                    break;
+                }
+                State.setState(count);
+                if (State.getState()) {
+                    text = text + placeholder;
+                }
+            } else if (State.getState()) {
+                text = text + initialText.charAt(i);
             }
         }
-        if (leftBrace.size() != rightBrace.size()){
-            return text;
-        } else if (text.contains("<")) {
-            String initialText = text;
-            leftBrace.add(text.length());
-            text ="";
-            int leftSign = -1;
-            int rightSign = 0;
-            for (int i = 0; i < rightBrace.size(); i++) {
-                if (leftBrace.get(i) < rightBrace.get(i) && leftSign < rightSign ){
-                    leftSign = leftBrace.get(i);
-                    text = text + initialText.substring(rightSign , leftSign);
-                }
-                if (rightBrace.get(i) > leftBrace.get(i) && rightBrace.get(i) < leftBrace.get(i + 1)){
-                    rightSign = rightBrace.get(i) + 1;
-                    text = text + placeholder;
-                    if (i == rightBrace.size() - 1){
-                        text = text + initialText.substring(rightSign);
-                    }
-                }
-            }
+        if (count > 0){
+            text = initialText;
         }
         return text;
+    }
+}
+class State {
+    private static boolean isOpening;
+    public State() {
+        isOpening = false;
+    }
+    public static void setState(int count) {
+        isOpening = count > 0;
+    }
+    public static boolean getState() {
+        return !isOpening;
     }
 }
