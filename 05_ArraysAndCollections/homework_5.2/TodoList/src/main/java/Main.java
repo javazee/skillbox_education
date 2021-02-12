3647f0d41ede164b2e625a24a5156478d13f5469
Eduard <edza92@mail.ru>
2021-02-12 21:32:10 +0500

    Homework 5.2 done

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,104 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        System.out.println("Введите задачу для добавления в список:");
+        Scanner getTask = new Scanner(System.in);
+        String task = getTask.nextLine();
+        todoList.add(task);
+        System.out.println("Введите задачу для добавления в список:");
+        task = getTask.nextLine();
+        System.out.println("Присвоить приоритет для задачи? 1 - да; 0 - нет");
+        Scanner action  = new Scanner(System.in);
+        int number = action.nextInt();
+        if (number == 0) {
+            todoList.add(task);
+        } else {
+            System.out.println("Введите приоритет для задачи от 1 до " + (todoList.getTodos().size() + 1) + " ,где 1 - первый пункт в списке");
+            number = action.nextInt();
+            if (number == todoList.getTodos().size() + 1){
+                todoList.add(task);
+            } else {
+                todoList.add(number - 1, task);
+            }
+        }
+        System.out.println("Нажмите: 0 - если хотите вывести список активных задач\n" +
+                "\t\t1 - если хотите добавить новую задачу\n" +
+                "\t\t2 - если хотите удалить задачу\n" +
+                "\t\t3 - если хотите заменить задачу\n" +
+                "\t\t4 - если хотите выйти из программы");
+        number = action.nextInt();
+        while (number != 4) {
+            if (number == 0) {
+                System.out.println("Список активных задач: ");
+                for (int i = 0; i < todoList.getTodos().size(); i ++){
+                    System.out.println((i + 1) + " - " + todoList.getTodos().get(i));
+                }
+                System.out.println("Нажмите: 1 - если хотите добавить новую задачу\n" +
+                        "\t\t2 - если хотите удалить задачу\n" +
+                        "\t\t3 - если хотите заменить задачу\n" +
+                        "\t\t4 - если хотите выйти из программы");
+                number = action.nextInt();
+            }
+            if (number == 1) {
+                System.out.println("Введите задачу для добавления в список: ");
+                task = getTask.nextLine();
+                System.out.println("Присвоить приоритет для задачи? 1 - да; 0 - нет");
+                number = action.nextInt();
+                if (number == 0) {
+                    todoList.add(task);
+                } else {
+                    System.out.println("Введите приоритет для задачи от 1 до " + (todoList.getTodos().size() + 1) + " ,где 1 - первый пункт в списке");
+                    number = action.nextInt();
+                    if (number == todoList.getTodos().size() + 1){
+                        todoList.add(task);
+                    } else {
+                        todoList.add(number - 1, task);
+                    }
+                }
+                System.out.println("Нажмите: 0 - если хотите вывести список активных задач\n" +
+                        "\t\t1 - если хотите добавить новую задачу\n" +
+                        "\t\t2 - если хотите удалить задачу\n" +
+                        "\t\t3 - если хотите заменить задачу\n" +
+                        "\t\t4 - если хотите выйти из программы");
+                number = action.nextInt();
+            }
+            if (number == 2){
+                System.out.println("Введите номер удаляемой задачи из списка ниже: ");
+                for (int i = 0; i < todoList.getTodos().size(); i ++){
+                    System.out.println((i + 1) + " - " + todoList.getTodos().get(i));
+                }
+                number = action.nextInt();
+                todoList.delete(number - 1);
+                System.out.println("Нажмите: 0 - если хотите вывести список активных задач\n" +
+                        "\t\t1 - если хотите добавить новую задачу\n" +
+                        "\t\t2 - если хотите удалить задачу\n" +
+                        "\t\t3 - если хотите заменить задачу\n" +
+                        "\t\t4 - если хотите выйти из программы");
+                number = action.nextInt();
+            }
+            if (number == 3){
+                System.out.println("Введите номер удаляемой задачи из списка ниже: ");
+                for (int i = 0; i < todoList.getTodos().size(); i ++){
+                    System.out.println((i + 1) + " - " + todoList.getTodos().get(i));
+                }
+                number = action.nextInt();
+                System.out.println("Введите новую задачу: ");
+                task = getTask.nextLine();
+                todoList.edit(task, number - 1);
+                System.out.println("Нажмите: 0 - если хотите вывести список активных задач\n" +
+                        "\t\t1 - если хотите добавить новую задачу\n" +
+                        "\t\t2 - если хотите удалить задачу\n" +
+                        "\t\t3 - если хотите заменить задачу\n" +
+                        "\t\t4 - если хотите выйти из программы");
+                number = action.nextInt();
+            }
+        }
+
+
     }
 }
--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -1,29 +1,45 @@
 import java.util.ArrayList;
 
 public class TodoList {
-
+    ArrayList<String> todoList = new ArrayList<>();
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index < todoList.size()) {
+            todoList.add(index, todo);
+        } else {
+            todoList.add(todo);
+            System.out.println("Неверный ввод! Задача помещена в конец списка");
+        }
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index < todoList.size()) {
+            todoList.set(index, todo);
+        } else {
+            System.out.println("Неверный ввод! Список остался без изменений");
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index < todoList.size()) {
+            todoList.remove(index);
+        } else {
+            System.out.println("Неверный ввод! Список остался без изменений");
+        }
     }
-
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        return todoList;
     }
 
 }
