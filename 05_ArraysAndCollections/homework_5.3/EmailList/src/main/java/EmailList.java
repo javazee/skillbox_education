72b20ec94d5798376ed6d8cd144527e2913b1e93
edza92@mail.ru <edza92@mail.ru>
2021-02-15 16:31:19 +0500

    Homework 5.3 done

--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,20 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class EmailList {
-
+    TreeSet<String> emailList = new TreeSet<>();
     public void add(String email) {
         // TODO: валидный формат email добавляется
+        if (Pattern.matches("[A-Za-z-_0-9]+@[A-Za-z0-9-]+\\.[A-Za-z]+", email)) {
+            if (!emailList.contains(email.toLowerCase())) {
+                emailList.add(email);
+            }
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return new ArrayList<>(emailList);
     }
 
 }
--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
+
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
         test@test.com
@@ -19,15 +21,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        EmailList emailList = new EmailList();
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
             //TODO: write code here
-            
+            if (input.contains("ADD")){
+                Pattern pattern = Pattern.compile("(?<=ADD)\\s+[A-Za-z-_0-9]+@[A-Za-z0-9-]+\\.[A-Za-z]+");
+                Matcher match = pattern.matcher(input);
+                if (input.contains("ADD")) {
+                    if (match.find()) {
+                        emailList.add(match.group().trim().toLowerCase());
+                    } else {
+                        System.out.println(Main.WRONG_EMAIL_ANSWER);
+                    }
+                }
+            } else if (input.contains("LIST")){
+                System.out.println(emailList.getSortedEmails());
+            }
         }
     }
 }
