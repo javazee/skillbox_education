2958988988a4db6e6b06787a574cd830ba067f1d
Eduard <edza92@mail.ru>
2021-02-15 22:36:48 +0500

    5.4 started

--- 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/Main.java
@@ -1,5 +1,22 @@
+import net.sf.saxon.expr.Component;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-
+        PhoneBook phoneBook = new PhoneBook();
+        System.out.println("Введите номер, имя или команду:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Pattern patternName = Pattern.compile("[А-Яа-я]+");
+        Pattern patternPhone = Pattern.compile("[\\+\\d]+.+");
+        Matcher name = patternName.matcher(input);
+        Matcher phone = patternPhone.matcher(input);
+        if (Pattern.matches("/[А-Яа-я]+\\s+(\\+)?([78]\\s*\\(\\d{3}\\)\\s*)?\\d\\s*[\\d\\s-]+\\d\\s*\\z/gm", input)){
+            phoneBook.addContact(phone.toString(), name.toString());
+        }
+        System.out.println(phoneBook.getAllContacts());
     }
 }
--- 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/PhoneBook.java
+++ 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,13 +1,33 @@
+import java.util.HashMap;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
 
+    TreeMap<String, Integer> phonebook = new TreeMap<>();
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
-    }
+        Pattern space = Pattern.compile("\\D");
+        Matcher number = space.matcher(phone);
+        Pattern pattern = Pattern.compile("\\b([78])?\\d{10}\\b");
+        Matcher onlyDigit = pattern.matcher(number.replaceAll(""));
+        if (onlyDigit.find()){
+            if (onlyDigit.toString().length() == 10) {
+                phone = "7" + onlyDigit.group();
+            } else {
+                phone = onlyDigit.group().replaceFirst("[78]?", "7");
+            }
+            phonebook.put(name, Integer.parseInt(phone));
+            System.out.println(phonebook.keySet());
+        } else {
+            System.out.println("Неверный формат ввода");
+        }
 
+    }
     public String getNameByPhone(String phone) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
@@ -23,7 +43,8 @@ public class PhoneBook {
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+        Set<String> copyPhoneBook = phonebook.keySet();
+        return copyPhoneBook;
     }
 
 }
