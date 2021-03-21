c1509488fb4bbed730d0230c3a7c4886cee9db82
Eduard <edza92@mail.ru>
2021-03-21 20:22:18 +0500

    Homework 11.1 done

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,23 @@
+import java.io.File;
+
 public class FileUtils {
+    private static long size = 0;
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                for (File files : file.listFiles()) {
+                    if (files.isDirectory()) {
+                        calculateFolderSize(files.getAbsolutePath());
+                    } else if (files.isFile()) {
+                        size = size + files.length();
+                    }
+                }
+                return size;
+            } else return file.length();
+        } else {
+            throw new IllegalArgumentException("Папка или файл по указанному пути не существует");
+        }
     }
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        while (true){
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите путь до папки:");
+                String path = scanner.nextLine();
+                System.out.println("Размер содержимого по адресу " + path + " " + FileUtils.calculateFolderSize(path) + " байт");
+            } catch (IllegalArgumentException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
