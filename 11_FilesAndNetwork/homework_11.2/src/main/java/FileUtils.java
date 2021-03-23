8ebfb54556d40b4b715364306db43030c3e7b60d
edza92@mail.ru <edza92@mail.ru>
2021-03-23 12:01:01 +0500

    11.2 done

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,39 @@
+import java.io.*;
+import java.util.Objects;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+        File file = new File(sourceDirectory);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                for (File files : Objects.requireNonNull(file.listFiles())) {
+                    File newFile = new File(destinationDirectory, files.getName());
+                    if (files.isDirectory()) {
+                        newFile.mkdir();
+                        copyFolder(files.getAbsolutePath(), newFile.getAbsolutePath());
+                    } else if (files.isFile()) {
+                        newFile.createNewFile();
+                        try (FileInputStream sourceFile = new FileInputStream(files.getAbsolutePath());
+                             FileOutputStream destinationFile = new FileOutputStream(newFile.getAbsolutePath())){
+                            byte[] buffer = new byte[sourceFile.available()];
+                            sourceFile.read(buffer);
+                            destinationFile.write(buffer);
+                        }
+                    }
+                }
+        } else {
+                File newFile = new File(destinationDirectory, file.getName());
+                newFile.createNewFile();
+                try (FileInputStream sourceFile = new FileInputStream(file.getAbsolutePath());
+                     FileOutputStream destinationFile = new FileOutputStream(newFile.getAbsolutePath())){
+                    byte[] buffer = new byte[sourceFile.available()];
+                    sourceFile.read(buffer);
+                    destinationFile.write(buffer);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Папка или файл по указанному пути не существует");
+        }
     }
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите путь папки, файлы с которой необходимо копировать:");
+            String inputPath = scanner.nextLine();
+            System.out.println("Введите путь папки, в которую необходимо копировать файлы:");
+            String outputPath = scanner.nextLine();
+            FileUtils.copyFolder(inputPath, outputPath);
+        } catch (IllegalArgumentException | IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
