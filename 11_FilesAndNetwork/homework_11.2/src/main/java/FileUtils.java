8d59ff1210f6d145ad19ec94ac1464d770e1a5be
edza92@mail.ru <edza92@mail.ru>
2021-03-25 14:35:46 +0500

    added comparing source's and destination's path of folders

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
@@ -7,29 +9,51 @@ public class FileUtils {
         File file = new File(sourceDirectory);
         if (file.exists()) {
             if (file.isDirectory()) {
+                File newFile;
+                //Ситуация, если папка копируется в директорию, в которой сама же и находится
+                if (file.getParent().equals(destinationDirectory)) {
+                    newFile = new File(destinationDirectory, file.getName() + " - копия");
+                    newFile.mkdir();
+                    copyFolder(file.getAbsolutePath(), newFile.getAbsolutePath());
+                }
                 for (File files : Objects.requireNonNull(file.listFiles())) {
-                    File newFile = new File(destinationDirectory, files.getName());
+                    newFile = new File(destinationDirectory, files.getName());
                     if (files.isDirectory()) {
                         newFile.mkdir();
                         copyFolder(files.getAbsolutePath(), newFile.getAbsolutePath());
                     } else if (files.isFile()) {
                         newFile.createNewFile();
                         try (FileInputStream sourceFile = new FileInputStream(files.getAbsolutePath());
-                             FileOutputStream destinationFile = new FileOutputStream(newFile.getAbsolutePath())){
+                             FileOutputStream destinationFile = new FileOutputStream(newFile.getAbsolutePath())) {
                             byte[] buffer = new byte[sourceFile.available()];
                             sourceFile.read(buffer);
                             destinationFile.write(buffer);
                         }
                     }
                 }
-        } else {
-                File newFile = new File(destinationDirectory, file.getName());
+            } else {
+                File newFile;
+                //Ситуация, если файл копируется в директорию, в которой сам и находится
+                if (file.getParent().equals(destinationDirectory)) {
+                    Pattern name = Pattern.compile(".+(?=\\.)");
+                    Pattern fileType = Pattern.compile("(?<=\\.).+");
+                    Matcher matchName = name.matcher(file.getName());
+                    Matcher fileTypeName = fileType.matcher(file.getName());
+                    matchName.find();
+                    fileTypeName.find();
+                    newFile = new File(destinationDirectory, (matchName.group() + " - копия." + fileTypeName.group()));
+                } else {
+                    newFile = new File(destinationDirectory, file.getName());
+                }
                 newFile.createNewFile();
-                try (FileInputStream sourceFile = new FileInputStream(file.getAbsolutePath());
-                     FileOutputStream destinationFile = new FileOutputStream(newFile.getAbsolutePath())){
+                try {
+                    FileInputStream sourceFile = new FileInputStream(file.getAbsolutePath());
+                    FileOutputStream destinationFile = new FileOutputStream(newFile.getAbsolutePath());
                     byte[] buffer = new byte[sourceFile.available()];
                     sourceFile.read(buffer);
                     destinationFile.write(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         } else {
