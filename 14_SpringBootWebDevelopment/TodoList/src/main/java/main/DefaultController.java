104325ee859f8e31266bea92eea5171898a0530b
edza92@mail.ru <edza92@mail.ru>
2021-07-14 22:45:55 +0500

    17.4 done

--- 14_SpringBootWebDevelopment/TodoList/src/main/java/main/DefaultController.java
+++ 14_SpringBootWebDevelopment/TodoList/src/main/java/main/DefaultController.java
@@ -3,17 +3,25 @@ package main;
 import main.model.Task;
 import main.model.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Controller
 public class DefaultController {
 
     @Autowired
     TaskRepository taskRepository;
+
+    @Value("${company.name}")
+    String companyName;
+
+    String currentDate = getCurrentDate();
 
 
     @RequestMapping("/")
@@ -25,6 +33,12 @@ public class DefaultController {
         }
         model.addAttribute("tasks", tasks);
         model.addAttribute("tasksCount", tasks.size());
+        model.addAttribute("currentDate", currentDate);
+        model.addAttribute("company", companyName);
         return "index";
+    }
+
+    public String getCurrentDate() {
+        return new SimpleDateFormat("dd.MM.yy").format(new Date());
     }
 }
