5ed2afb869d87e59682993882d8515d67e678436
edza92 <edza92@mail.ru>
2021-07-29 20:14:35 +0500

    18.3.2 done

--- 18_NoSQL/MongoDB/src/main/java/Main.java
+++ 18_NoSQL/MongoDB/src/main/java/Main.java
@@ -1,5 +1,4 @@
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.BsonDocument;
@@ -9,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
 
@@ -16,7 +16,6 @@ public class Main {
         MongoClient mongoClient = new MongoClient("127.0.0.1" , 27017);
         MongoDatabase database = mongoClient.getDatabase("local");
         MongoCollection<Document> collection = database.getCollection("students");
-
         try {
             List<String> lines = Files.readAllLines(Paths.get("src/main/resources/mongo.csv"));
             String delimiter1 = ",(?=\\d)";
@@ -26,7 +25,7 @@ public class Main {
                 String[] elements2 = elements1[1].split(delimiter2);
                 Document document = new Document()
                         .append("Name", elements1[0])
-                        .append("Age", elements2[0])
+                        .append("Age", Integer.parseInt(elements2[0]))
                         .append("Courses", elements2[1]);
                 collection.insertOne(document);
             }
@@ -35,7 +34,11 @@ public class Main {
         }
         System.out.println("Общее количество студентов: " + collection.countDocuments());
         BsonDocument query = BsonDocument.parse("{Age: {$gt: 40}}");
-
+        System.out.println("Количество студентов старше 40: " + collection.countDocuments(query));
+        BsonDocument sortByAge = BsonDocument.parse("{Age: 1}");
+        System.out.println("Самый молодой студент: " + Objects.requireNonNull(collection.find().sort(sortByAge).first()).get("Name"));
+        BsonDocument sortByAgeReversed = BsonDocument.parse("{Age: -1}");
+        System.out.println("Список курсов самого старого студента: " + Objects.requireNonNull(collection.find().sort(sortByAgeReversed).first()).get("Courses"));
         collection.drop();
     }
 }
